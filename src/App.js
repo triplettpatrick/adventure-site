0613bfcf3882cee382d23d9cce7e144acf646412
triplettpatrick <patrick_triplett@alumni.brown.edu>
2023-03-26 15:56:08 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection';
+import Home from './components/pages/Home'
 
 function App() { //functional component which uses 'react-router-dom' package to set up routing in a web application
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar /> 
+        <Home />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home}/>
         </Routes>
       </Router>
     </>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,20 @@
-import { Button } from 'bootstrap'
-import React from 'react'
+import { Button } from 'bootstrap';
+import React from 'react';
+import './HeroSection.css';
+
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="./videos/video-2.mp4" autoPlay loop muted />
+        {/* <video src="%PUBLIC_URL%/videos/video-1.mp4" autoPlay loop muted /> */}
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
             <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">GET STARTED</Button>
+            <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">WATCH TRAILER <i className='far fa-play-circle'></i></Button>
         </div>
     </div>
   )
 }
 
-export default HeroSection
+export default HeroSection;
